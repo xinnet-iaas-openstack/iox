7d3ccf490067e3056a75adef555747bbb17acbeb
lichuntao <lichuntao@xinnet.com>
2012-02-06 16:52:14 +0800

    xinnet openstackx create

---
+++ Makefile
@@ -0,0 +1,25 @@
+PYTHON=`which python`
+DESTDIR=/
+PROJECT=openstackx
+VERSION=0.2.0
+
+all:
+	@echo "make source - Create source package"
+	@echo "make install - Install on local system"
+	@echo "make buildrpm - Generate a rpm package"
+	@echo "make clean - Get rid of scratch and byte files"
+
+source:
+	$(PYTHON) setup.py sdist $(COMPILE)
+
+install:
+	$(PYTHON) setup.py install --root $(DESTDIR) $(COMPILE)
+
+buildrpm:
+	$(PYTHON) setup.py bdist_rpm --post-install=rpm/postinstall --pre-uninstall=rpm/preuninstall
+
+clean:
+	$(PYTHON) setup.py clean
+	$(MAKE) -f $(CURDIR)/debian/rules clean
+	rm -rf build/ MANIFEST
+	find . -name '*.pyc' -delete
---
+++ openstackx/admin/flavors.py
@@ -0,0 +1,44 @@
+from openstackx.api import base
+from openstackx import compute # fixme import from jacobian
+
+
+class Flavor(compute.Flavor):
+    def __repr__(self):
+        return "<Flavor: %s>" % self.name
+
+    def delete(self, purge=False):
+        self.manager.delete(self.id, purge)
+
+#    @property
+#    def memory_mb(self):
+#        return self._info['memory_mb']
+
+
+class FlavorManager(compute.FlavorManager):
+    resource_class = Flavor
+
+    def create(self, name, memory_mb, vcpus, local_gb, flavorid,
+               swap=0, rxtx_factor=1):
+
+        body = {}
+        body['flavor'] = {}
+        body['flavor']['name'] = name
+        body['flavor']['memory_mb'] = memory_mb
+        body['flavor']['vcpus'] = vcpus
+        body['flavor']['local_gb'] = local_gb
+        body['flavor']['flavorid'] = flavorid
+        body['flavor']['swap'] = swap
+        body['flavor']['rxtx_factor'] = rxtx_factor
+
+        return self._create('/admin/flavors', body, "flavor")
+
+    def delete(self, id, purge=False):
+        self._delete("/admin/flavors/%s?purge=%s" % (id, purge))
+
+    def list(self):
+        """
+        Get a list of all flavors.
+
+        :rtype: list of :class:`Flavor`.
+        """
+        return self._list("/admin/flavors", "flavors")
